438f6db0f0979cc68d5df4a4d465ce8edfc4ad36
ROHIT SINGH <rohitsi2252@gmail.com>
2026-07-18 11:40:55 +0530

    9

--- src/factors.java
+++ src/factors.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class factors {
     public static void main(String[] args) {
-        factors1(20);
+        factors3(20);
     }
     static void factors1(int n){
         for(int i=1; i<=n; i++){
@@ -14,11 +16,28 @@ public class factors {
         for(int i=1; i<=Math.sqrt(n); i++){
             if(n % i == 0){
                 if(n/i == i){
-                    System.out.println(i);
+                    System.out.print(i);
                 }else {
-                    System.out.println(i + " " + n / i);
+                    System.out.print(i + " " + n / i);
                 }
             }
+        }
+    }
+    //both time and space with be 0(sqrt(n))
+    static void factors3(int n){
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=1; i<=Math.sqrt(n); i++){
+            if(n % i == 0){
+                if(n/i == i){
+                    System.out.print(i+ " ");
+                }else {
+                    System.out.print(i + " ");
+                    list.add(n/i);
+                }
+            }
+        }
+        for(int i = list.size() -1; i >= 0; i--){
+            System.out.print(list.get(i)+ " ");
         }
     }
 }
