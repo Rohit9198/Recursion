7c94029d04974339db3a64ec055d20a073925242
ROHIT SINGH <rohitsi2252@gmail.com>
2026-07-25 12:35:11 +0530

    .

--- src/Arrays/find.java
+++ src/Arrays/find.java
@@ -1,11 +1,16 @@
 package Arrays;
 
+import java.util.ArrayList;
+
 public class find {
     public static void main(String[] args) {
-       int[] arr = {2, 3, 1, 4, 5};
+       int[] arr = {2, 3, 1,4, 4, 5};
         System.out.println(find(arr, 4, 0));
         System.out.println(findIndex(arr, 4, 0));
-        System.out.println(findIndexLast(arr,4,0));
+        System.out.println(findIndexLast(arr,4,arr.length-1));
+        findAllIndex(arr, 4, 0);
+        System.out.println(list);
+
     }
     static boolean find(int[] arr, int target, int index){
         if(index == arr.length){
@@ -33,6 +38,17 @@ public class find {
         }else{
             return findIndexLast(arr, target, index - 1);
         }
+    }
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndex(int[] arr, int target, int index){
+        if(index == arr.length){
+            return;
+        }
+        if(arr[index] == target) {
+            list.add(index);
+        }
+            findAllIndex(arr, target, index+1);
+
 
     }
 }
