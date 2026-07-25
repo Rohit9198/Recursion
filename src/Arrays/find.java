9043c40f68718912ed3cdeaf0e394b45ac5f0cbb
ROHIT SINGH <rohitsi2252@gmail.com>
2026-07-25 13:03:09 +0530

    ,

--- src/Arrays/find.java
+++ src/Arrays/find.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class find {
     public static void main(String[] args) {
        int[] arr = {2, 3, 1,4, 4, 5};
-        System.out.println(find(arr, 4, 0));
-        System.out.println(findIndex(arr, 4, 0));
-        System.out.println(findIndexLast(arr,4,arr.length-1));
-        findAllIndex(arr, 4, 0);
-        System.out.println(list);
-
+//        System.out.println(find(arr, 4, 0));
+//        System.out.println(findIndex(arr, 4, 0));
+//        System.out.println(findIndexLast(arr,4,arr.length-1));
+//        findAllIndex(arr, 4, 0);
+//        System.out.println(list);
+        System.out.println(findAllIndex2(arr, 4, 0));
     }
     static boolean find(int[] arr, int target, int index){
         if(index == arr.length){
@@ -49,6 +49,22 @@ public class find {
         }
             findAllIndex(arr, target, index+1);
 
+
+    }
+    static ArrayList<Integer> findAllIndex2(int[] arr, int target, int index) {
+        ArrayList<Integer> list = new ArrayList<>();
+            if (index == arr.length) {
+                return list;
+            }
+            // this will contain answer for that function only
+            if (arr[index] == target) {
+                list.add (index);
+            }
+            ArrayList<Integer> ansFromBelowCalls = findAllIndex2(arr, target, index + 1);
+
+            list.addAll(ansFromBelowCalls);
+
+            return list;
 
     }
 }
