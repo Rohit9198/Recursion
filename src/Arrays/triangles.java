3b50aec875fd6c71aecc2754dfeef7cb0af84f56
ROHIT SINGH <rohitsi2252@gmail.com>
2026-07-26 22:42:13 +0530

    /

---
+++ src/Arrays/triangles.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class triangles {
+    public static void main(String[] args) {
+        triangles(4, 0);
+    }
+    static void triangles(int r, int c){
+        if(r == 0){
+            return;
+        }
+        if(c < r){
+            System.out.print("*");
+            triangles(r, c+1);
+        }else{
+            System.out.println();
+            triangles(r-1, 0);
+        }
+    }
+}
