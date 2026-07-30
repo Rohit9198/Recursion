f4582fea1e0921099c1029dc2b17965fdd506b64
ROHIT SINGH <rohitsi2252@gmail.com>
2026-07-30 19:29:28 +0530

    d

--- src/String/dice.java
+++ src/String/dice.java
@@ -1,8 +1,11 @@
 package String;
 
+import java.util.ArrayList;
+
 public class dice {
     public static void main(String[] args) {
-       dice("", 4);
+       //dice("", 4);
+        System.out.println(diceRet("", 4));
     }
     static void dice(String p, int target){
         if(target == 0){
@@ -13,5 +16,18 @@ public class dice {
         for(int i = 1; i <= 6 && i <= target; i++){
             dice(p + i, target - i);
         }
+    }
+    static ArrayList<String> diceRet(String p, int target){
+        if(target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 1; i <= 6 && i <= target; i++){
+            list.addAll(diceRet(p + i, target - i));
+        }
+        return list;
     }
 }
