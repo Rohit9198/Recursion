6e27868a29f7aad5e916280069afea53e56a25b7
ROHIT SINGH <rohitsi2252@gmail.com>
2026-07-30 18:40:21 +0530

    d

--- src/String/permutations.java
+++ src/String/permutations.java
@@ -1,8 +1,13 @@
 package String;
 
+import java.util.ArrayList;
+
 public class permutations {
     public static void main(String[] args) {
-       permutations("", "abc");
+       //permutations("", "abc");
+
+        ArrayList<String> ans = permutationsList("", "abc");
+        System.out.println(ans);
     }
     static void permutations(String p, String up){
         if(up.isEmpty()){
@@ -17,5 +22,23 @@ public class permutations {
             permutations(f + ch + s, up.substring(1));
 
         }
+    }
+    static ArrayList<String> permutationsList(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+
+        //local call
+         ArrayList<String> ans = new ArrayList<>();
+        for(int i =0; i<= p.length(); i++){
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            ans.addAll(permutationsList(f + ch + s, up.substring(1)));
+
+        }
+        return ans;
     }
 }
