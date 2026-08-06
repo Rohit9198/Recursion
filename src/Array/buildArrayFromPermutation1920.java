b727d60965f7e4dd428ebc663f025204e328446c
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-06 23:51:44 +0530

    4

---
+++ src/Array/buildArrayFromPermutation1920.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.Arrays;
+
+public class buildArrayFromPermutation1920 {
+    public static void main(String[] args) {
+        int[] nums={0, 2, 1, 5, 3, 4};
+        int[] ans = buildArray(nums);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    public static int[] buildArray(int[] nums){
+        int n = nums.length;
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
