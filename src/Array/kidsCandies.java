42e3f8303da4cf4d5520980ba68d42680f1bab79
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-08 12:06:35 +0530

    3

---
+++ src/Array/kidsCandies.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class kidsCandies {
+    public static void main(String[] args) {
+       int[] candies = {2, 3, 5, 1, 3 };
+       int extraCandies = 3;
+       List <Boolean> ans = kidsWithCandies(candies, extraCandies);
+        System.out.println(ans);
+    }
+    public static List <Boolean> kidsWithCandies(int[] candies, int extraCandies){
+        List <Boolean> result = new ArrayList<>();
+        int max =0;
+        for(int candy : candies){
+            if(candy > max){
+                max = candy;
+            }
+        }
+        for(int candy : candies){
+            if(candy + extraCandies >= max){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
