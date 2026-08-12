691cd1e343d474eb6f9163e7fb52fca5d4621a7f
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-12 13:00:57 +0530

    5

--- src/Array/hashmap.java
+++ src/Array/hashmap.java
@@ -1,6 +1,7 @@
 package Array;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class hashmap {
     public static void main(String[] args) {
@@ -10,7 +11,20 @@ public class hashmap {
         map.put("Karan", 99);
         map.put("Rohit", 94);
 
-        System.out.println(map.get("Karan"));
+        //System.out.println(map.get("Karan"));
+        //System.out.println(map.getOrDefault("Apoorv", 78));
+        System.out.println(map.containsKey("Rohit"));
+
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(56);
+        set.add(91);
+        set.add(12);
+        set.add(43);
+        set.add(56);
+        set.add(2);
+
+        System.out.println(set);
 
     }
 }
