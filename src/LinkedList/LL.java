6cbdc2f9a057c32ef7b85713f5cb086f709cd01d
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-14 11:25:26 +0530

    4

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -68,6 +68,24 @@ public class LL {
         temp.next = node;
         size++;
     }
+    public int deleteLast(){
+        if(size <= 1){
+            return deleteFirst();
+        }
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+        return val;
+    }
+
+    public Node get(int index){
+        Node node = head;
+        for(int i =0; i< index; i++){
+            node = node.next;
+        }
+        return node;
+    }
 
     public int deleteFirst(){
         int val = head.value;
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -11,7 +11,8 @@ public class Main {
         list.insert(100,3);
         list.display();
         System.out.println(list.deleteFirst());
-
+        list.display();
+        System.out.println(list.deleteLast());
         list.display();
     }
 }
