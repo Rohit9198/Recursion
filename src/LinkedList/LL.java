c851b48da8da97a1c8597ffe340240abf573f264
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-14 11:19:00 +0530

    6

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -69,6 +69,15 @@ public class LL {
         size++;
     }
 
+    public int deleteFirst(){
+        int val = head.value;
+        head = head.next;
+        if(head == null){
+            tail = null;
+        }
+        size--;
+        return val;
+    }
     public void display() {
 
         Node temp = head;
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -9,6 +9,8 @@ public class Main {
         list.insertFirst(17);
         list.insertLast(99);
         list.insert(100,3);
+        list.display();
+        System.out.println(list.deleteFirst());
 
         list.display();
     }
