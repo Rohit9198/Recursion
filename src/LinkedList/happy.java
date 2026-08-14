fbb5e22dd5f2ee0246a0bc8bb691714cba27affa
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-15 02:47:02 +0530

    happy

---
+++ src/LinkedList/happy.java
@@ -0,0 +1,42 @@
+package LinkedList;
+
+public class happy {
+
+    public static void main(String[] args) {
+
+        happy obj = new happy();
+
+        int n = 19;
+
+        boolean result = obj.isHappy(n);
+
+        System.out.println(result);
+    }
+
+    public boolean isHappy(int n) {
+
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private int findSquare(int number) {
+
+        int ans = 0;
+
+        while (number > 0) {
+            int rem = number % 10;
+            ans += rem * rem;
+            number /= 10;
+        }
+
+        return ans;
+    }
+}
