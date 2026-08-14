9c447217b34570237d4f94d4a56f800764b9d4ad
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-14 13:07:00 +0530

    6

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -16,14 +16,22 @@ public class Main {
 //        list.display();
 //        System.out.println(list.delete(2));
 //        list.display();
+//
+//        DLL list = new DLL();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(99);
+//        list.insert(8,65);
+//
+//        list.display();
 
-        DLL list = new DLL();
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(17);
-        list.insertLast(99);
-        list.insert(8,65);
+        CLL list = new CLL();
+        list.insert(23);
+        list.insert(3);
+        list.insert(19);
+        list.insert(75);
 
         list.display();
     }
