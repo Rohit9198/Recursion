87fac8eff7c560a80eb684b4b7e2fd00bdc5aa61
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-14 12:42:20 +0530

    5

--- src/LinkedList/DLL.java
+++ src/LinkedList/DLL.java
@@ -33,6 +33,53 @@ public class DLL {
         System.out.println("START");
     }
 
+    public void insertLast(int val){
+        Node node = new Node(val);
+        Node last = head;
+
+        node.next = null;
+
+        if(head == null){
+            node.prev = null;
+            head = node;
+            return ;
+        }
+
+        while(last.next != null){
+            last = last.next;
+        }
+        last.next = node;
+        node.prev = last;
+    }
+
+    public Node find(int value){
+        Node node = head;
+        while(node != null){
+            if(node.val == value){
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public void insert(int after, int val){
+        Node p = find(after);
+
+        if(p == null){
+            System.out.println("does not exist");
+            return;
+        }
+        Node node = new Node(val);
+        node.next = p.next;
+        p.next = node;
+        node.prev = p;
+        if(node.next != null){
+            node.next.prev = node;
+        }
+
+    }
+
     public void displayRev(){
         Node node = head;
         while(node != null){
