8f6e92e27068785249a945c567562e133c9580f0
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-16 14:11:33 +0530

    reverse Linked List

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -276,4 +276,36 @@ public class LL {
             bubbleSort(row - 1, 0);
         }
     }
+    // recursion reverse
+    private void reverse(Node node){
+       if(node == tail){
+           head = tail;
+           return;
+       }
+
+       reverse(node.next);
+
+       tail.next = node;
+       tail = node;
+       tail.next = null;
+    }
+    //in place reversal of linked list
+    public void reverse(){
+        if(size < 2){
+            return;
+        }
+        Node prev = null;
+        Node present = head;
+        Node next = present.next;
+
+        while(present != null){
+            present.next = prev;
+            prev = present;
+            present = next;
+            if(next != null){
+                next = next.next;
+            }
+        }
+        head = prev;
+    }
 }
